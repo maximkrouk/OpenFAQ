a0e995229e20c748f0a16500008a1e0086702194
Maxim Krouk <id.maximkrouk@gmail.com>
2020-01-28 14:45:20 +0300

    User controller added.
    - User creation implemented.

---
+++ Sources/App/Controllers/User/UserController.swift
@@ -0,0 +1,25 @@
+//
+//  UserController.swift
+//  App
+//
+//  Created by Maxim Krouk on 1/27/20.
+//
+
+import Fluent
+import Vapor
+
+class UserController {
+    func createUser(on req: Request) throws -> EventLoopFuture<User> {
+        try User.Create.validate(req)
+        let create = try req.content.decode(User.Create.self)
+        guard create.password == create.confirmPassword else {
+            throw Abort(.badRequest, reason: "Passwords did not match")
+        }
+        let user = try User(firstName: create.firstName,
+                            lastName: create.lastName,
+                            email: create.email,
+                            passwordHash: Bcrypt.hash(create.password),
+                            projects: [])
+        return user.save(on: req.db).map { user }
+    }
+}
